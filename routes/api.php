766ec88005efd26a2515810e7776da72dc9f5f0f
Rifqi Akram <rifqiakram57@gmail.com>
2020-06-24 11:48:55 +0700

    Add middleware authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,22 +11,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
-Route::group(['middleware' => 'auth:api'], function(){
-	Route::post('details', 'API\UserController@details');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('details', 'API\UserController@details');
 
 });
 
-Route::get('buku', 'BukuController@index');
-Route::post('buku', 'BukuController@create');
-Route::put('/buku/{id}', 'BukuController@update');
-Route::delete('/buku/{id}', 'BukuController@delete');
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('buku', 'BukuController@index');
+    Route::post('buku', 'BukuController@create');
+    Route::put('/buku/{id}', 'BukuController@update');
+    Route::delete('/buku/{id}', 'BukuController@delete');
 
-Route::get('sewa', 'PenyewaanController@index');
-Route::post('sewa', 'PenyewaanController@create');
+    Route::get('sewa', 'PenyewaanController@index');
+    Route::post('sewa', 'PenyewaanController@create');
+});
 // Route::put('/sewa/{id}', 'PenyewaanController@update');
 // Route::delete('/sewa/{id}', 'PenyewaanController@delete');
-
